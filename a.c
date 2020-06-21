e3ba624fa329bbe594d2cf1fffc524aef2529724
Viktor Borodin <vikbrdin@gmail.com>
2020-06-21 16:55:01 +0300

    with output 	modified:   a.c

--- a.c
+++ a.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 
 int main(){
-
+	printf("int arr[2][3] = { {2, 8, 4}, {5, 6, 7} }\n\n");
+	printf("[2][8][4]\n[5][6][7]\n\n");
 	int arr[2][3] = { {2, 8, 4}, {5, 6, 7} };
+	printf("*pointer = arr\n");
 	int *pointer = arr;
 	
-	printf("%d\n", ++pointer[0]);
+	printf("++pointer[0]: %d\n\n", ++pointer[0]);
 
+// создание и заполнение тройного динамического 
+// числами
 	int **ptr = (int**)malloc(sizeof(int*)*2);
 
 	for(int i = 0; i < 3; i++)
@@ -15,9 +19,12 @@ int main(){
 		ptr[i] = (int*)malloc(sizeof(int)*3);
 		for(int k = 0; k < 3; k++ )
 		{
-			ptr[i][k] = i*10 + k;
+			ptr[i][k] = i*10 + 2*k;
 		}
 	}
+// -------- конец заполнения
+
+ printf("динамический двойной массив 3x3 ptr\n с таким содержимым:\n\n");
 
  for(int i = 0; i < 3; i++)
  {
@@ -27,11 +34,18 @@ int main(){
 	 }
 	 printf("\n");
  }
- printf("адрес 1эл 1 строки: ptr[0] %p\n", ptr[0]);
+ printf("\nадрес 1эл 1 строки: ptr[0] %p\n", ptr[0]);
  printf("ptr[0][0]: %d\n", ptr[0][0]);
  ptr[0]++;
  printf("ptr[0]++\n");
  printf("адрес 2эл 1 строки: ptr[0] %p\n", ptr[0]);
- printf("ptr[0][0]: %d\n", ptr[0][0]);
+ printf("ptr[0][0]: %d\n\n", ptr[0][0]);
 
+ pointer = arr[1];
+ printf("pointer = arr[2]\n");
+
+ for(int i=0; i < 3; i++)
+ {
+	 printf("pointer[%d]: %d\n", i, pointer[i]);
+ }
 }
